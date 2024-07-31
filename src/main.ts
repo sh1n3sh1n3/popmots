b0a55b9f417d36da057b7bf244c0a569dc6a25a8
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2024-07-31 18:16:31 +0200

    Add font

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@fontsource-variable/nunito/wght.css';
+import './assets/styles/styles.scss';
 
 const app = createApp(App)
 
