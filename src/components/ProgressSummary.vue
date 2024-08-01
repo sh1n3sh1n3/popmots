2c54f4959e2b7f6ecf24844e65860375fda55a60
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2024-08-01 14:53:35 +0200

    Add progress bar and summary

---
+++ src/components/ProgressSummary.vue
@@ -0,0 +1,83 @@
+<script lang="ts" setup>
+interface Props {
+  summary: Array<
+    {
+      valueNow: number
+      valueMax: number
+      type: 'reviewed' | 'learning' | 'new'
+    }
+  >
+
+}
+defineProps<Props>();
+
+</script>
+<template>
+  <div class="progress-summary">
+    <article
+      v-for="total in summary"
+      :key="total.type"
+      class="progress-summary__total"
+    >
+      <span>
+        {{ `${total.valueNow} / ${total.valueMax}` }}
+      </span>
+      <h4
+        :class="['progress-summary__title', `progress-summary__title--${total.type}`]"
+      >
+        {{ total.type }}
+      </h4>
+    </article>
+  </div>
+</template>
+
+
+<style lang="scss">
+.progress-summary {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  margin-top: var(--space-xs);
+  border: var(--border);
+  border-radius: var(--border-radius);
+  width: 100%;
+  font-weight: 700;
+
+  &__total {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: var(--space-3xs);
+    width: 100%;
+    padding: var(--space-xs);
+    color: var(--secondary);
+    font-size: var(--font-2xl);
+
+    &:first-child {
+      border-right: var(--border);
+    }
+
+    &:last-child {
+      border-left: var(--border);
+    }
+  }
+
+  &__title {
+    font-size: var(--font-xs);
+    text-transform: uppercase;
+
+    &--reviewed {
+      color: var(--green-dark);
+    }
+
+    &--learning {
+      color: var(--red-dark);
+    }
+
+    &--new {
+      color: var(--yellow-dark);
+    }
+  }
+
+}
+</style>
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const props = defineProps<{
+	name: 'settings',
+	size?: 'l',
+}>();
+</script>
+
+<template>
+	<svg :class="['svg', { 'svg--l': size === 'l' }]">
+		<use :href="`/sprite.svg#${props.name}`"></use>
+	</svg>
+</template>
+
+
+<style lang="scss">
+svg {
+	width: var(--font-2xl);
+	height: var(--font-2xl);
+
+	&.svg--l {
+		width: var(--font-5xl);
+		height: var(--font-5xl);
+	}
+}
+</style>
