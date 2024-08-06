f7d36b7767c7a422c7c74131dfb57ef069ec891a
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2024-08-06 12:01:26 +0200

    Add bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import generateIconNames from './plugins/generate-icon-names'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     generateIconNames('./public/sprite.svg'),
+    visualizer()
   ],
   resolve: {
     alias: {
