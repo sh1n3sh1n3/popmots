029465b2055a2f8426c5643804ea38ebb9c38789
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2024-08-13 17:49:02 +0200

    Use R2 bucket

--- functions/api/dictionary/[[word]].ts
+++ functions/api/dictionary/[[word]].ts
@@ -1,9 +1,20 @@
-import { dictionary } from "most-common-words-fr-dict-generator";
-export function onRequest(context) {
-    if (context.params.word?.length === 1) {
-        const word = context.params.word[0];
-        const wordEntries = dictionary.get(word) ?? [];
-        return new Response(JSON.stringify(wordEntries))
+import type { Dictionary, DictionaryWord } from "most-common-words-fr-dict-generator/types";
+export async function onRequest({ params, env }) {
+    const word = params.word;
+
+    if (word) {
+        const dictionaryJson = await env.DICTIONARY;
+        if (dictionaryJson) {
+            const entries = Object.entries(dictionaryJson) as unknown as Array<[string, Array<DictionaryWord>]>;
+            const dictionary: Dictionary = entries.reduce((acc, curr) => {
+                acc.set(curr[0], curr[1]);
+                return acc;
+            }, new Map() as Dictionary);
+            const wordEntries = dictionary.get(word);
+            if (wordEntries) {
+                return new Response(JSON.stringify(wordEntries))
+            }
+        }
     }
-    return new Response('Not found', { status: 404 })
+    return new Response(`Word "${params.word}" not found`, { status: 404 })
 }
