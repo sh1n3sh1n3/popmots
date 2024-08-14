5de59e993b244d5176940a08a8f95bdba30e7b17
sh1n3sh1n3 <sh1n3sh1n3.03@gmail.com>
2024-08-14 19:06:02 +0200

    Delete log

--- functions/api/dictionary/[word].ts
+++ functions/api/dictionary/[word].ts
@@ -3,7 +3,6 @@ import type { WordEntries } from '../../../src/types';
 export async function onRequestGet({ params, env }) {
     const word = decodeURI(params.word);
     if (word) {
-        console.log(`Word: ${word}`);
         const wordEntries = await env.DB.prepare(`SELECT * FROM Dictionary WHERE word = "${word}";`).all();
         if (wordEntries) {
             const wordEntriesFormatted: WordEntries[] = wordEntries.results.map(entry => {
